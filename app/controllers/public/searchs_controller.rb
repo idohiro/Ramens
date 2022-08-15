ded316ff25953666c5d50450c38327b90559897d
idohiro <idohiro0509@gmail.com>
2022-08-15 13:06:37 +0000

    インデント修正

--- app/controllers/public/searchs_controller.rb
+++ app/controllers/public/searchs_controller.rb
@@ -1,7 +1,6 @@
 class Public::SearchsController < ApplicationController
   def search
     @range = params[:range]
-
     if @range == "Customer"
       @customers = Customer.looks(params[:search], params[:word])
       redirect_to public_search_result_path()
@@ -10,7 +9,6 @@ class Public::SearchsController < ApplicationController
        redirect_to public_search_result_path()
     end
   end
-
   def search_result
     @customers = Customer.looks(params[:search], params[:word])
     @ramens = Ramen.looks(params[:search], params[:word])
